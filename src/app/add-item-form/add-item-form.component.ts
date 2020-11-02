7a8b64333ba3c3212d7bb83c3b2a63c3313c1aed
Daniela Sinclair <dasfluid@gmail.com>
2020-11-02 14:12:59 +0000

    Tidied stylesheets

--- src/app/add-item-form/add-item-form.component.ts
+++ src/app/add-item-form/add-item-form.component.ts
@@ -18,10 +18,7 @@ export class AddItemFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    // if item has a value
     if (this.item) {
-      // this means that an existing item object was passed into this component
-      // therefore this is not a new item
       this.isNewItem = false;
     } else {
       this.isNewItem = true;
